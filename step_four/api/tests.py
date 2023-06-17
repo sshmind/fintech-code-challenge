cfd3a2564c9aee899c538bc0894d0ddfb8d916d0
sshmind <saeidworkspace@gmail.com>
2023-06-17 05:33:54 +0330

    add tests for step four

--- step_four/api/tests.py
+++ step_four/api/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from unittest.mock import patch
+import json
 
-# Create your tests here.
+
+class BustStockAPITests(TestCase):
+
+
+    @patch('utils.redis.Redis')
+    def test_accepted_response(self, mock_redis):
+
+        mock_client = mock_redis.return_value
+
+        user_data = {'credit': 100}
+        stock_data = {'price': [10, 20, 30]}
+        mock_client.get.side_effect = lambda key: json.dumps(user_data) if key == 'user1' else json.dumps(stock_data)
+
+        client = APIClient()
+        response = client.post('/api/buy-stock/', data={'user': 'user1', 
+                                                                  'stockname': 'stock1',
+                                                                  'quantity': 1})
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['message'], 'ACCEPTED')
+    
+    @patch('utils.redis.Redis')
+    def test_low_user_credit(self, mock_redis):
+        mock_client = mock_redis.return_value
+
+        user_data = {'credit': 0}
+        stock_data = {'price': [10, 20, 30]}
+        mock_client.get.side_effect = lambda key: json.dumps(user_data) if key == 'user1' else json.dumps(stock_data)
+
+        client = APIClient()
+        response = client.post('/api/buy-stock/', data={'user': 'user1', 
+                                                                  'stockname': 'stock1',
+                                                                  'quantity': 1})
+        
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data['message'], "user doesn't have enough credit!")
+    
+    def test_api_serializer_for_invalid_user_account(self):
+
+        client = APIClient()
+        response = client.post('/api/buy-stock/', data={'user': 'user5', 
+                                                                  'stockname': 'stock1',
+                                                                  'quantity': 1})
+        
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['user'][0], 'Invalid user account!')
+
+    def test_api_serializer_for_invalid_stock_name(self):
+
+        client = APIClient()
+        response = client.post('/api/buy-stock/', data={'user': 'user1', 
+                                                                  'stockname': 'stock9',
+                                                                  'quantity': 1})
+        
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['stockname'][0], 'Invalid stock name!')
