26ca0b4894f9a3ffbc34c79ec13e1ae35ef1d6bc
sshmind <saeidworkspace@gmail.com>
2023-06-17 09:02:43 +0330

    user redis name for redis_client

--- step_one/redis_handler.py
+++ step_one/redis_handler.py
@@ -6,7 +6,7 @@ import json
 
 def save_data_to_redis(csv_row, current_time_str):
     
-    redis_client = Redis(host='127.0.0.1', port='6379', db=0)
+    redis_client = Redis(host='redis', port='6379', db=0)
     DATA_KEYS = ['stock1', 'stock2', 'stock3']
     process = None
     
